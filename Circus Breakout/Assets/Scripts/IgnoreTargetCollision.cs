8788f29226103f50f2f95a743f056978f65f9431
Huadong Zhang <407108459@qq.com>
2019-10-30 17:23:21 -0400

    Personal Change

---
+++ Circus Breakout/Assets/Scripts/IgnoreTargetCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreTargetCollision : MonoBehaviour
+{
+    public Collider2D target;
+    
+    void Start()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), target);
+    }
+
+    
+}
