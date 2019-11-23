d23be4ed910fe5e6d1b76c7796719aee7d59050e
Lanzarote_ZRY <33143405+LanzaroteZRY@users.noreply.github.com>
2019-11-23 17:59:48 -0500

    Test 3
    Add Test Scene

---
+++ Circus Breakout/Assets/Scripts/WindArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    public Vector2 Force = Vector2.zero;
+    private Collider2D Mouse;
+
+    private void OnTriggerStay2D(Collider collision)
+    {
+        if(collision.tag.Equals("Mouse"))
+        //Rigidbody2D body = object.attachedRigidbody;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
