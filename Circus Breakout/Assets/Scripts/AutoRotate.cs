d236e66e1043de3ab6f5935843e2efa9e7ef1df2
zhd407108459 <407108459@qq.com>
2019-11-24 19:28:03 +0800

    Change

---
+++ Circus Breakout/Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    public float rotateSpeed;
+    public bool clockwise;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (clockwise)
+        {
+            transform.Rotate(new Vector3(0, 0, -rotateSpeed * Time.deltaTime));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, rotateSpeed * Time.deltaTime));
+        }
+    }
+}
