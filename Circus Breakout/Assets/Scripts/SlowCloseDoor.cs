be86daa0e78191492769754c49809fdda8d61a4b
zhd407108459 <407108459@qq.com>
2019-11-26 08:34:49 +0800

    Fix when the board conflicts with box in Level 10

--- Circus Breakout/Assets/Scripts/SlowCloseDoor.cs
+++ Circus Breakout/Assets/Scripts/SlowCloseDoor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SlowCloseDoor : MonoBehaviour
 {
+    public bool isOnlyBlockedByTriggerItem;
+
     [HideInInspector]
     public int contacts;
 
@@ -14,13 +16,17 @@ public class SlowCloseDoor : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag.Equals("Elephant")|| collision.gameObject.tag.Equals("Mouse") || collision.gameObject.tag.Equals("TriggerItem"))
+        if((collision.gameObject.tag.Equals("Elephant") && !isOnlyBlockedByTriggerItem) || (collision.gameObject.tag.Equals("Mouse") && !isOnlyBlockedByTriggerItem) || collision.gameObject.tag.Equals("TriggerItem"))
         {
             //Debug.Log("1:"+(transform.position.y - collision.gameObject.transform.position.y).ToString());
             //Debug.Log(transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y);
+            //Debug.Log((transform.position.y - collision.gameObject.transform.position.y) - 0.5f * (transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y));
             if ((transform.position.y - collision.gameObject.transform.position.y) - 0.5f * (transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y) > -1.0f) 
             {
-                
+                contacts++;
+            }
+            else if ((collision.gameObject.transform.position.y - transform.position.y) - 0.5f * (transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y) > -1.0f)
+            {
                 contacts++;
             }
         }
@@ -28,11 +34,15 @@ public class SlowCloseDoor : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("Elephant") || collision.gameObject.tag.Equals("Mouse") || collision.gameObject.tag.Equals("TriggerItem"))
+        if ((collision.gameObject.tag.Equals("Elephant") && !isOnlyBlockedByTriggerItem) || (collision.gameObject.tag.Equals("Mouse") && !isOnlyBlockedByTriggerItem) || collision.gameObject.tag.Equals("TriggerItem"))
         {
             if ((transform.position.y - collision.gameObject.transform.position.y) - 0.5f * (transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y) > -1.0f)
             {
 
+                contacts--;
+            }
+            else if ((collision.gameObject.transform.position.y - transform.position.y) - 0.5f * (transform.localScale.y * GetComponent<BoxCollider2D>().size.y + collision.gameObject.transform.localScale.y * collision.gameObject.GetComponent<BoxCollider2D>().size.y) > -1.0f)
+            {
                 contacts--;
             }
         }
